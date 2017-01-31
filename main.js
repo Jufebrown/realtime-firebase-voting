4cd7f990446c78c2a74e5acddaf0679734805759
Jufebrown <jufebrown@gmail.com>
2017-01-31 12:05:54 -0600

    use firebase databaseJS api

--- main.js
+++ main.js
@@ -8,8 +8,6 @@
     messagingSenderId: "330870997829"
   });
 
-
-
   document
     .querySelectorAll('.choice button')
     .forEach(btn => btn.addEventListener('click', onVote))
@@ -21,14 +19,17 @@
     const url = 'https://angularfire-practice.firebaseio.com/votes.json'
 
     //go get current counts
-    fetch(url)
-      .then(res => res.json())
+    // fetch(url)
+    firebase.database().ref('votes').once('value')
+      .then(snap => snap.val())
+      // .then(res => res.json())
       .then(data => {
         const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1 //ternary
-        fetch(url, {
-          method: 'PATCH',
-          body: JSON.stringify({[voteFor]: newCount})
-        })
+        // fetch(url, {
+        //   method: 'PATCH',
+        //   body: JSON.stringify({[voteFor]: newCount})
+        // })
+        return firebase.database().ref('votes').update({[voteFor]: newCount})
         .then(()=>{
           //show current vote totals
           document.querySelectorAll('h3').forEach(h => {
