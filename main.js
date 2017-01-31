8d0ab66f927c33b5c43930fa612f7fcd74165549
Jufebrown <jufebrown@gmail.com>
2017-01-31 11:50:33 -0600

    rest

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
 {
-  document.querySelector('.choice_a button').addEventListener('click', onVote)
-  document.querySelector('.choice_b button').addEventListener('click', onVote)
+  // Initialize Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyCMMNNzEP5HY_JMOGQoam-xH5AZqtvzhAk",
+    authDomain: "angularfire-practice.firebaseapp.com",
+    databaseURL: "https://angularfire-practice.firebaseio.com",
+    storageBucket: "angularfire-practice.appspot.com",
+    messagingSenderId: "330870997829"
+  });
 
-  function onVote () {
-    //hide buttons
-    document.querySelectorAll('button').forEach(btn => btn.remove());
-    //show current vote totals
+
+
+  document
+    .querySelectorAll('.choice button')
+    .forEach(btn => btn.addEventListener('click', onVote))
+
+  function onVote (evt) {
     //submit vote
+      //what button
+    const voteFor = evt.target.closest('.choice').dataset.value
+    const url = 'https://angularfire-practice.firebaseio.com/votes.json'
 
+    //go get current counts
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1 //ternary
+        fetch(url, {
+          method: 'PATCH',
+          body: JSON.stringify({[voteFor]: newCount})
+        })
+        .then(()=>{
+          //show current vote totals
+          document.querySelectorAll('h3').forEach(h => {
+            const total = Object.values(data).reduce((acc, val) => acc + val)
+            const current = data[h.closest('.choice').dataset.value]
+            h.innerText = Math.round(current / total * 100) + "%"
+          })
+        })
+      })
+      //hide buttons
+      .then(document.querySelectorAll('button').forEach(btn => btn.remove()))
+      .catch(console.error)
   }
 }
+
+
+
+// Array methods
+
+// forEach - return is undefined
+// map - return is the array mangled ex: ['apple','banana'] => ['APPLE', 'BANANA']
+// filter - returns the array with possibly fewer items
+// reduce - returns a single value
